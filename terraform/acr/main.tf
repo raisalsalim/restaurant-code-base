db5cdd938de8853f6373d4ff8735c9c64e09bb7b
raisalsalim <raisalsalim333@gmail.com>
2024-11-22 21:40:13 +0530

    added tag

--- terraform/acr/main.tf
+++ terraform/acr/main.tf
@@ -5,14 +5,20 @@ resource "azurerm_container_registry" "acr" {
   location                 = var.resource_group_location
   sku                      = "Basic"
   admin_enabled            = true
+
+  tags = {
+    environment = "production"
+  }  
 }
 
 # Assign the "AcrPull" role to the AKS managed identity for accessing ACR
-resource "azurerm_role_assignment" "aks_acr_pull" {
-  principal_id         = var.aks_principal_id
-  role_definition_name = "AcrPull"
-  scope                = azurerm_container_registry.acr.id
-}
+#resource "azurerm_role_assignment" "aks_acr_pull" {
+#  depends_on = [azurerm_container_registry.acr]
+#
+#  principal_id         = var.aks_principal_id
+#  role_definition_name = "AcrPull"
+#  scope                = azurerm_container_registry.acr.id
+#}
 
 resource "null_resource" "aks_attach_acr" {
   depends_on = [azurerm_container_registry.acr]
