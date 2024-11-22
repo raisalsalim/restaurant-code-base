64f4880466493b5550329c2dce0e3029312d7b56
raisalsalim <raisalsalim333@gmail.com>
2024-11-22 11:03:29 +0530

    Revert "added ingress controller and ingress"
    This reverts commit d9560be2cd115c2729f547227d8a8f659cb9f2d0.

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -25,22 +25,22 @@ resource "azurerm_subnet" "db_subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-#resource "azurerm_network_security_group" "aks_nsg" {
-#  name                = "aks-nsg"
-#  location            = azurerm_resource_group.rg.location
-#  resource_group_name = azurerm_resource_group.rg.name
-#}
+resource "azurerm_network_security_group" "aks_nsg" {
+  name                = "aks-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-#resource "azurerm_network_security_rule" "aks_allow_inbound" {
-#  name                        = "allow_inbound"
-#  priority                    = 100
-#  direction                   = "Inbound"
-#  access                      = "Allow"
-#  protocol                    = "Tcp"
-#  source_port_range           = "*"
-#  destination_port_range      = "443"
-#  source_address_prefix       = "*"
-#  destination_address_prefix  = "*"
-#  resource_group_name         = azurerm_resource_group.rg.name
-#  network_security_group_name = azurerm_network_security_group.aks_nsg.name
-#}
+resource "azurerm_network_security_rule" "aks_allow_inbound" {
+  name                        = "allow_inbound"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.aks_nsg.name
+}
